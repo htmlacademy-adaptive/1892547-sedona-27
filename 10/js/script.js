671ab249df090de8ab7d33e14a16c6730b8caec0
Keks <keks@htmlacademy.ru>
2023-01-11 19:41:02 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/script.js
@@ -0,0 +1,1 @@
+const nav=document.querySelector(".nav"),logo=document.querySelector(".main-header__logo-img"),toggle=document.querySelector(".nav__toggle");nav.classList.remove("nav--nojs"),logo.classList.remove("main-header__logo-img--nojs"),toggle.addEventListener("click",(()=>{nav.classList.contains("nav--opened")?nav.classList.remove("nav--opened"):nav.classList.add("nav--opened")}));
